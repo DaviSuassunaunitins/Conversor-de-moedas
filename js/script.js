71cdd396c886d7e20caa8416ed92d620d3d6d6eb
DaviSuassunaunitins <davisuassuna@unitins.br>
2025-04-17 17:52:25 -0300

    version 3
    Tem muita coisa pra fazer ainda

---
+++ js/script.js
@@ -0,0 +1,16 @@
+async function trocar() {
+    let valor = parseFloat(document.getElementById('valor').value);
+    let deMoeda = document.getElementById('de-moeda').value;
+    let paraMoeda = document.getElementById('para-moeda').value; 
+
+    if (deMoeda == paraMoeda) {
+        // document.getElementById('resul').innerHTML
+        document.getElementById('resul').innerHTML = "Escolha moedas diferentes";
+        return;
+    }
+    else {
+        while (NamedNodeMap) {
+            
+        }
+    }
+}
